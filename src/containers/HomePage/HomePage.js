60d341382a9df054acff67fd6f982b6473014ada
Bart <bartst010395@gmail.com>
2019-02-25 16:26:48 +0100

    Images optimalization

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -8,12 +8,12 @@ import Modal from '../../components/UI/Modal/Modal';
 import Button from '../../components/UI/Button/Button';
 import ScrollToTopOnMount from '../../shared/ScrollToTopOnMount';
 
-import saleBg from '../../assets/home_page/Sale.jpeg';
-import slideOne from '../../assets/home_page/slide_1.jpeg';
-import slideTwo from '../../assets/home_page/slide_2.jpeg';
-import slideThree from '../../assets/home_page/slide_3.jpeg';
-import slideFour from '../../assets/home_page/slide_4.jpeg';
-import slideFive from '../../assets/home_page/slide_5.jpeg';
+import saleBg from '../../assets/home_page/Sale.jpg';
+import slideOne from '../../assets/home_page/slide_1.jpg';
+import slideTwo from '../../assets/home_page/slide_2.jpg';
+import slideThree from '../../assets/home_page/slide_3.jpg';
+import slideFour from '../../assets/home_page/slide_4.jpg';
+import slideFive from '../../assets/home_page/slide_5.jpg';
 
 const slides = [];
 slides.push(slideOne, slideTwo, slideThree, slideFour, slideFive);
